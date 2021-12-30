48c6fd95e203cfdc46dc2b9941b5ab510b225890
xiao0yy <xiao0yy@gmail.com>
2021-12-30 14:39:14 +0800

    Update zsh

--- shell.plugin.zsh
+++ shell.plugin.zsh
@@ -1,1 +1,28 @@
-source ${0:A:h}/init
+function fnm() {
+    unset -f fnm
+    eval "$(fnm env)"
+    fnm $@
+}
+
+function navi() {
+    unset -f navi
+    eval "$(navi widget zsh)"
+    navi $@
+}
+
+function pyenv() {
+    unset -f pyenv
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    pyenv $@
+}
+
+function sdk() {
+    unset -f sdk
+    source "$SDKMAN_DIR/bin/sdkman-init.sh"
+    sdk $@
+}
+
+typeset -gU path
+path[1,0]=("$HOME/bin" "$HOME/.local/bin" "$HOME/.cargo/bin")
