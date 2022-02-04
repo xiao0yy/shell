0cffb5ead0696fa6ece891faa81d20e89721ff08
xiao0yy <xiao0yy@gmail.com>
2022-02-04 14:05:57 +0800

    Update shell.plugin.zsh

--- shell.plugin.zsh
+++ shell.plugin.zsh
@@ -31,3 +31,17 @@ path[1,0]=("$HOME/bin" "$HOME/.local/bin" "$HOME/.cargo/bin")
 export HISTSIZE=999999999
 export SAVEHIST=$HISTSIZE
 export HISTFILE="$HOME/.zsh_history"
+
+export MCFLY_FUZZY=2
+export MCFLY_RESULTS=32
+export MCFLY_INTERFACE_VIEW=BOTTOM
+export MCFLY_RESULTS_SORT=LAST_RUN
+
+alias cat='bat --style plain,header,grid'
+alias l='exa -lbghH'
+alias ll='exa -lbghH'
+alias la='exa -labghH'
+alias ld='exa -lDbghH'
+alias lad='exa -laDbghH'
+alias http='xh'
+alias https='xh'
